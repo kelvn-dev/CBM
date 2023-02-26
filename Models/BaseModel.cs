2005abcc6fb8cc34d2d9878c773b559b9cc41a0d
kelvn <huonggiang7657@gmail.com>
2023-02-26 20:54:27 +0700

    Setup BaseModel with general field ID and createdTime

---
+++ Models/BaseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CBM.Models {
+  public class BaseModel {
+
+    public BaseModel() {
+      id = Guid.NewGuid();
+    }
+
+    public object this[string propertyName] {
+      get { return this.GetType().GetProperty(propertyName).GetValue(this, null); }
+      set {
+        value = value == DBNull.Value ? null : value;
+        this.GetType().GetProperty(propertyName).SetValue(this, value, null);
+      }
+    }
+
+    private Guid _id;
+    private DateTime _createdTime;
+
+    public Guid id {
+      get => _id;
+      set => _id = value;
+    }
+
+    public DateTime createdTime {
+      get => _createdTime;
+      set => _createdTime = value;
+    }
+  }
+}
